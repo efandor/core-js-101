928cd80d1bc3ad20ba31fa7499249bfafbeea14b
efandor <firstdir@gmail.com>
2020-12-04 09:40:47 +0300

    fix: push for solve autotest problem

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -463,8 +463,8 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return new Array(end - start + 1).fill(0).map((_, i) => start + i);
 }
 
 /**
@@ -478,8 +478,8 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return Array.from(new Set(arr));
 }
 
 /**
